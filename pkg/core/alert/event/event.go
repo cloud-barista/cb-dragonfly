bdca13b0af92e1932c4181eeb9b6b3bba16bdf71
inno-cloudbarista <69338107+inno-cloudbarista@users.noreply.github.com>
2021-06-14 11:34:58 +0900

    [PULL 모듈] PULL 모듈 적용 (#89)
    * [PULL 모듈] PULL-Manager 에이전트 관리 기능 및 PULL 기본 동작 로직 추가
    * [PULL 모듈] PULL-Caller 모듈 에이전트 온디멘드 API 호출 및 InfluxDB 저장 로직 추가
    * [PULL 모듈] PULL-Caller 모듈 에이전트 헬스체크 로직 추가
    * [PULL 모듈] PULL-Caller 모듈 에이전트 헬스체크 리프레시 기능 추가
    
    Co-authored-by: hyokyung.kim <hyokyung.kim@innogrid.com>
    Co-authored-by: pjini <64886639+pjini@users.noreply.github.com>
    Co-authored-by: inno_pjh <inno_pjh@innogrid.com>

--- pkg/core/alert/event/event.go
+++ pkg/core/alert/event/event.go
@@ -2,16 +2,15 @@ package event
 
 import (
 	"encoding/json"
-	"strings"
 
+	"github.com/cloud-barista/cb-dragonfly/pkg/cbstore"
 	"github.com/cloud-barista/cb-dragonfly/pkg/core/alert/types"
-	"github.com/cloud-barista/cb-dragonfly/pkg/localstore"
 )
 
 func CreateEventLog(eventLog types.AlertEventLog) error {
 	var eventLogArr []types.AlertEventLog
 
-	eventLogStr := localstore.GetInstance().StoreGet(eventLog.Id)
+	eventLogStr := cbstore.GetInstance().StoreGet(eventLog.Id)
 
 	if eventLogStr != "" {
 		// Get event log array
@@ -29,7 +28,7 @@ func CreateEventLog(eventLog types.AlertEventLog) error {
 	if err != nil {
 		return err
 	}
-	err = localstore.GetInstance().StorePut(eventLog.Id, string(newEventLogBytes))
+	err = cbstore.GetInstance().StorePut(eventLog.Id, string(newEventLogBytes))
 	if err != nil {
 		return err
 	}
@@ -37,28 +36,21 @@ func CreateEventLog(eventLog types.AlertEventLog) error {
 }
 
 func ListEventLog(taskId string, logLevel string) ([]types.AlertEventLog, error) {
-	var eventLogArr []types.AlertEventLog
-	eventLogStr := localstore.GetInstance().StoreGet(taskId)
-	if eventLogStr == "" {
-		return []types.AlertEventLog{}, nil
-	}
+	eventLogArr := []types.AlertEventLog{}
+	eventLogStr := cbstore.GetInstance().StoreGet(taskId)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//if eventLogStr == nil {
+	//	return eventLogArr, nil
+	//}
 	err := json.Unmarshal([]byte(eventLogStr), &eventLogArr)
 	if err != nil {
 		return nil, err
 	}
-	if logLevel == "" {
-		return eventLogArr, nil
-	}
-
-	filterdEventLogArr := []types.AlertEventLog{}
-	for _, log := range eventLogArr {
-		if strings.EqualFold(log.Level, logLevel) {
-			filterdEventLogArr = append(filterdEventLogArr, log)
-		}
-	}
-	return filterdEventLogArr, nil
+	return eventLogArr, nil
 }
 
 func DeleteEventLog(taskId string) error {
-	return localstore.GetInstance().StoreDelete(taskId)
+	return cbstore.GetInstance().StoreDelete(taskId)
 }
