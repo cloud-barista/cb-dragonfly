94837ac3eb6b018babea5c382b2adbdfdcc06a17
Jin-Whee-Park <best888810@naver.com>
2021-07-05 18:05:19 +0900

    [기능 개선] 모니터링 메트릭 저장/조회 이름 통일 - PUSH, PULL 모니터링 메트릭 이름 통일

--- pkg/collector/aggregator.go
+++ pkg/collector/aggregator.go
@@ -2,14 +2,16 @@ package collector
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/cloud-barista/cb-dragonfly/pkg/config"
+	"github.com/cloud-barista/cb-dragonfly/pkg/util"
 	"sort"
 	"strconv"
 	"time"
 
 	"github.com/cloud-barista/cb-dragonfly/pkg/cbstore"
 	"github.com/cloud-barista/cb-dragonfly/pkg/metricstore/influxdb/v1"
-	"github.com/cloud-barista/cb-dragonfly/pkg/util"
 	"github.com/sirupsen/logrus"
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 
@@ -156,5 +158,93 @@ func (a *Aggregator) CalculateMetric(responseMap map[string]map[string]map[strin
 			resultMap[vmTopic].(map[string]interface{})["tagInfo"] = tagMap[vmTopic]
 		}
 	}
+	for topic, topicVal := range resultMap {
+		for metricName, metricVal := range topicVal.(map[string]interface{}) {
+			if metricName == "tagInfo" {
+				continue
+			}
+			tmpMetric, err := mappingOnDemandMetric(config.GetInstance().Monitoring.DefaultPolicy == types.PushPolicy, types.GetMetricType(metricName), metricVal.(map[string]interface{}))
+			if err != nil {
+				util.GetLogger().Error(err)
+				return nil, err
+			}
+			resultMap[topic].(map[string]interface{})[metricName] = tmpMetric
+		}
+	}
 	return resultMap, nil
+}
+
+func ConvertMonMetric(metric types.Metric, metricVal TelegrafMetric) (map[string]interface{}, error) {
+	metricMap := map[string]interface{}{}
+	metricMap["name"] = metricVal.Name
+	tagMap := map[string]interface{}{
+		"nsId":    metricVal.Tags["nsId"],
+		"mcisId":  metricVal.Tags["mcisId"],
+		"vmId":    metricVal.Tags["vmId"],
+		"osType":  metricVal.Tags["osType"],
+		"cspType": metricVal.Tags["cspType"],
+	}
+	metricMap["tags"] = tagMap
+
+	metricCols, err := mappingOnDemandMetric(config.GetInstance().Monitoring.DefaultPolicy == types.PushPolicy, metric, metricVal.Fields)
+	if err != nil {
+		return nil, err
+	}
+	metricMap["values"] = metricCols
+	metricMap["time"] = time.Now().UTC()
+	return metricMap, nil
+}
+
+func mappingOnDemandMetric(isPush bool, metric types.Metric, metricVal map[string]interface{}) (map[string]interface{}, error) {
+	// Metric 구조체 Map 변환
+	metricCols := map[string]interface{}{}
+
+	switch metric {
+	case types.Cpu:
+		metricCols["cpu_utilization"] = metricVal["usage_utilization"]
+		metricCols["cpu_system"] = metricVal["usage_system"]
+		metricCols["cpu_idle"] = metricVal["usage_idle"]
+		metricCols["cpu_iowait"] = metricVal["usage_iowait"]
+		metricCols["cpu_hintr"] = metricVal["usage_irq"]
+		metricCols["cpu_sintr"] = metricVal["usage_softirq"]
+		metricCols["cpu_user"] = metricVal["usage_user"]
+		metricCols["cpu_nice"] = metricVal["usage_nice"]
+		metricCols["cpu_steal"] = metricVal["usage_steal"]
+		metricCols["cpu_guest"] = metricVal["usage_guest"]
+		metricCols["cpu_guest_nice"] = metricVal["usage_guest_nice"]
+	case types.CpuFrequency:
+		metricCols["cpu_speed"] = metricVal["cur_freq"]
+	case types.Memory:
+		metricCols["mem_utilization"] = metricVal["used_percent"]
+		metricCols["mem_total"] = metricVal["total"]
+		metricCols["mem_used"] = metricVal["used"]
+		metricCols["mem_free"] = metricVal["free"]
+		metricCols["mem_shared"] = metricVal["shared"]
+		metricCols["mem_buffers"] = metricVal["buffered"]
+		metricCols["mem_cached"] = metricVal["cached"]
+	case types.Disk:
+		metricCols["disk_utilization"] = metricVal["used_percent"]
+		metricCols["disk_total"] = metricVal["total"]
+		metricCols["disk_used"] = metricVal["used"]
+		metricCols["disk_free"] = metricVal["free"]
+	case types.DiskIO:
+		metricCols["kb_read"] = metricVal["read_bytes"]
+		metricCols["kb_written"] = metricVal["write_bytes"]
+		metricCols["ops_read"] = metricVal["iops_read"]
+		metricCols["ops_write"] = metricVal["iops_write"]
+	case types.Network:
+		metricCols["bytes_in"] = metricVal["bytes_recv"]
+		metricCols["bytes_out"] = metricVal["bytes_sent"]
+		metricCols["pkts_in"] = metricVal["packets_recv"]
+		metricCols["pkts_out"] = metricVal["packets_sent"]
+	case types.None:
+	default:
+		if isPush {
+			break
+		}
+		err := errors.New("not found metric")
+		return nil, err
+	}
+
+	return metricCols, nil
 }
--- pkg/core/metric/ondemand_metric.go
+++ pkg/core/metric/ondemand_metric.go
@@ -73,81 +73,9 @@ func getVMOnDemandMonInfo(metric types.Metric, publicIP string) (map[string]inte
 	if err != nil {
 		return nil, err
 	}
-	resultMetric, err := convertMonMetric(metric, metricData[metric.ToAgentMetricKey()])
+	resultMetric, err := collector.ConvertMonMetric(metric, metricData[metric.ToAgentMetricKey()])
 	if err != nil {
 		return nil, err
 	}
 	return resultMetric, nil
-}
-
-func convertMonMetric(metric types.Metric, metricVal collector.TelegrafMetric) (map[string]interface{}, error) {
-	metricMap := map[string]interface{}{}
-	metricMap["name"] = metricVal.Name
-	tagMap := map[string]interface{}{
-		"nsId":    metricVal.Tags["nsId"],
-		"mcisId":  metricVal.Tags["mcisId"],
-		"vmId":    metricVal.Tags["vmId"],
-		"osType":  metricVal.Tags["osType"],
-		"cspType": metricVal.Tags["cspType"],
-	}
-	metricMap["tags"] = tagMap
-
-	metricCols, err := mappingOnDemandMetric(metric, metricVal.Fields)
-	if err != nil {
-		return nil, err
-	}
-	metricMap["values"] = metricCols
-	metricMap["time"] = time.Now().UTC()
-	return metricMap, nil
-}
-
-func mappingOnDemandMetric(metric types.Metric, metricVal map[string]interface{}) (map[string]interface{}, error) {
-	// Metric 구조체 Map 변환
-	metricCols := map[string]interface{}{}
-
-	switch metric {
-	case types.Cpu:
-		metricCols["cpu_utilization"] = metricVal["usage_utilization"]
-		metricCols["cpu_system"] = metricVal["usage_system"]
-		metricCols["cpu_idle"] = metricVal["usage_idle"]
-		metricCols["cpu_iowait"] = metricVal["usage_iowait"]
-		metricCols["cpu_hintr"] = metricVal["usage_irq"]
-		metricCols["cpu_sintr"] = metricVal["usage_softirq"]
-		metricCols["cpu_user"] = metricVal["usage_user"]
-		metricCols["cpu_nice"] = metricVal["usage_nice"]
-		metricCols["cpu_steal"] = metricVal["usage_steal"]
-		metricCols["cpu_guest"] = metricVal["usage_guest"]
-		metricCols["cpu_guest_nice"] = metricVal["usage_guest_nice"]
-	case types.CpuFrequency:
-		metricCols["cpu_speed"] = metricVal["cur_freq"]
-	case types.Memory:
-		metricCols["mem_utilization"] = metricVal["used_percent"]
-		metricCols["mem_total"] = metricVal["total"]
-		metricCols["mem_used"] = metricVal["used"]
-		metricCols["mem_free"] = metricVal["free"]
-		metricCols["mem_shared"] = metricVal["shared"]
-		metricCols["mem_buffers"] = metricVal["buffered"]
-		metricCols["mem_cached"] = metricVal["cached"]
-	case types.Disk:
-		metricCols["disk_utilization"] = metricVal["used_percent"]
-		metricCols["disk_total"] = metricVal["total"]
-		metricCols["disk_used"] = metricVal["used"]
-		metricCols["disk_free"] = metricVal["free"]
-	case types.DiskIO:
-		metricCols["kb_read"] = metricVal["read_bytes"]
-		metricCols["kb_written"] = metricVal["write_bytes"]
-		metricCols["ops_read"] = metricVal["iops_read"]
-		metricCols["ops_write"] = metricVal["iops_write"]
-	case types.Network:
-		metricCols["bytes_in"] = metricVal["bytes_recv"]
-		metricCols["bytes_out"] = metricVal["bytes_sent"]
-		metricCols["pkts_in"] = metricVal["packets_recv"]
-		metricCols["pkts_out"] = metricVal["packets_sent"]
-	case types.None:
-	default:
-		err := errors.New("not found metric")
-		return nil, err
-	}
-
-	return metricCols, nil
 }
