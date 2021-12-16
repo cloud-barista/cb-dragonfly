a6baa2e441a39422e1f1e0506ba49b8d0359ab0a
박재호 <inno_pjh@innogrid.com>
2021-12-16 11:38:21 +0900

    Release Tag(v0.5.0) Update

--- pkg/types/constant.go
+++ pkg/types/constant.go
@@ -66,7 +66,7 @@ const (
 	LabelKey  = "name"
 	Namespace = "dragonfly"
 	//CollectorImage = "docker.io/hojun121/collector:latest"
-	CollectorImage = "cloudbaristaorg/cb-dragonfly:0.4.4-collector"
+	CollectorImage = "cloudbaristaorg/cb-dragonfly:0.5.0-collector"
 )
 
 const (
