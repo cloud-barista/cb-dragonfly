232b5de0b5d45e292e2a5df99b4c30b0f1386158
pjini <pjini@innogrid.com>
2022-11-04 20:23:25 +0900

    [코르타도] MCK8S 모니터링 조회 API 분기 처리

--- pkg/api/rest/metric/mck8s/metric.go
+++ pkg/api/rest/metric/mck8s/metric.go
@@ -42,14 +42,6 @@ func GetMCK8SMonInfo(c echo.Context) error {
 	namespace := c.QueryParam("namespace")
 	pod := c.QueryParam("pod")
 
-	if strings.EqualFold(groupBy, types.Cluster) {
-		if len(node) > 0 {
-			if !strings.EqualFold(node, types.ALL) {
-				return echo.NewHTTPError(http.StatusBadRequest, rest.SetMessage(fmt.Sprintf("monitoring for single node is not supported with groupBy, %s", groupBy)))
-			}
-		}
-	}
-
 	if strings.EqualFold(groupBy, types.Node) {
 		if strings.EqualFold(node, types.ALL) || len(node) == 0 {
 			return echo.NewHTTPError(http.StatusBadRequest, rest.SetMessage("not supported node name for single node monitoring"))
@@ -65,6 +57,61 @@ func GetMCK8SMonInfo(c echo.Context) error {
 		if len(node) > 0 && len(namespace) > 0 {
 			return echo.NewHTTPError(http.StatusBadRequest, rest.SetMessage("not supported monitoring"))
 		}
+	}
+
+	if string(duration[len(duration)-1]) == "m" {
+		durationInt, _ := strconv.Atoi(duration[:len(duration)-1])
+		if durationInt < 2 {
+			return echo.NewHTTPError(404, rest.SetMessage("Error! Mininum duration time is 2m"))
+		}
+	}
+
+	dbInfo := types.DBMetricRequestInfo{
+		NsID:                nsId,
+		ServiceType:         types.MCK8S,
+		ServiceID:           mck8sId,
+		MetricName:          metricName,
+		MonitoringMechanism: strings.EqualFold(config.GetInstance().Monitoring.DefaultPolicy, types.PushPolicy),
+		Period:              period,
+		MCK8SReqInfo: types.MCK8SReqInfo{
+			GroupBy:   groupBy,
+			Node:      node,
+			Namespace: namespace,
+			Pod:       pod,
+		},
+		AggegateType: aggregateType,
+		Duration:     duration,
+	}
+
+	result, errCode, err := metric.GetMonInfo(dbInfo)
+	if errCode != http.StatusOK {
+		return echo.NewHTTPError(errCode, rest.SetMessage(err.Error()))
+	}
+	return c.JSON(http.StatusOK, result)
+}
+
+func GetMCK8SPerfMonInfo(c echo.Context) error {
+	// Path 파라미터 가져오기
+	nsId := c.Param("ns_id")
+	mck8sId := c.Param("mck8s_id")
+	metricName := c.Param("metric_name")
+	// Query 파라미터 가져오기
+	period := c.QueryParam("periodType")
+	aggregateType := c.QueryParam("statisticsCriteria")
+	duration := c.QueryParam("duration")
+	groupBy := c.QueryParam("groupBy")
+	node := c.QueryParam("node")
+	namespace := c.QueryParam("namespace")
+	pod := c.QueryParam("pod")
+
+	if strings.EqualFold(groupBy, types.Cluster) {
+		if len(node) > 0 {
+			if !strings.EqualFold(node, types.ALL) {
+				return echo.NewHTTPError(http.StatusBadRequest, rest.SetMessage(fmt.Sprintf("monitoring for single node is not supported with groupBy, %s", groupBy)))
+			}
+		}
+	} else {
+		return echo.NewHTTPError(http.StatusBadRequest, rest.SetMessage(fmt.Sprintf("unsupported monitoring groupBy '%s'", groupBy)))
 	}
 
 	if string(duration[len(duration)-1]) == "m" {
