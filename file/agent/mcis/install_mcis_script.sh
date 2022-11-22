75aea80306f2e66fe3f148423c11593fa83b1486
snchoi <snchoi@innogrid.com>
2022-11-22 14:46:12 +0900

    MCIS install, uninstall script refactoring

--- file/agent/mcis/install_mcis_script.sh
+++ file/agent/mcis/install_mcis_script.sh
@@ -1,36 +1,37 @@
-
 #!/bin/bash
 
 echo "[MCIS-Agent: Start to prepare a VM evaluation]"
 
-echo "[MCIS-Agent: Install sysbench]"
-sudo apt-get -y update
+echo "[MCIS-Agent: Initial Setting]"
+echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+sudo apt-get install -y -q
+
+sudo apt-get update
+sudo apt-get install -y locales locales-all
+sudo locale-gen ko_KR.UTF-8
+
 sudo apt-get install -y --no-install-recommends apt-utils
+sudo apt-get -y install dialog
+
+echo "[MCIS-Agent: Install sysbench]"
 sudo apt-get -y install sysbench
 
 echo "[MCIS-Agent: Install Ping]"
 sudo apt-get -y install iputils-ping
 
-echo "[MCIS-Agent: Install debconf-utils]"
-sudo apt-get -y install debconf-utils
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password psetri1234ak'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password psetri1234ak'
+echo "[MCIS-Agent: Set debconf]"
+sudo debconf-set-selections <<< 'mariadb-server mysql-server/root_password password psetri1234ak'
+sudo debconf-set-selections <<< 'mariadb-server mysql-server/root_password_again password psetri1234ak'
 
 echo "[MCIS-Agent: Install MySQL]"
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install mysql-server
+sudo apt-get -y install mariadb-server
 
 echo "[MCIS-Agent: Generate dump tables for evaluation]"
-
-mysql -u root -ppsetri1234ak -e "CREATE DATABASE sysbench;"
-mysql -u root -ppsetri1234ak -e "CREATE USER 'sysbench'@'localhost' IDENTIFIED BY 'psetri1234ak';"
-mysql -u root -ppsetri1234ak -e "GRANT ALL PRIVILEGES ON *.* TO 'sysbench'@'localhost' IDENTIFIED  BY 'psetri1234ak';"
+sudo mysql -u root -ppsetri1234ak -e "CREATE DATABASE sysbench;"
+sudo mysql -u root -ppsetri1234ak -e "CREATE USER 'sysbench'@'localhost' IDENTIFIED BY 'psetri1234ak';"
+sudo mysql -u root -ppsetri1234ak -e "GRANT ALL PRIVILEGES ON *.* TO 'sysbench'@'localhost' IDENTIFIED  BY 'psetri1234ak';"
 
 echo "[MCIS-Agent: Install tcpdump]"
 sudo apt-get -y install tcpdump
 
 echo "[MCIS-Agent: Preparation is done]"
-
-
-
-
-
