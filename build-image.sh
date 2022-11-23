d84c2058779e6e131e802e45be6d935f7f00970c
snchoi <sn0716@naver.com>
2022-11-23 12:46:50 +0900

    cb-dragonfly version name update

--- build-image.sh
+++ build-image.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # CB-Dragonfly Image
-sudo docker build -t cloudbaristaorg/cb-dragonfly:0.6.9 . --no-cache
-sudo docker push cloudbaristaorg/cb-dragonfly:0.6.9
+sudo docker build -t cloudbaristaorg/cb-dragonfly:0.7.0 . --no-cache
+sudo docker push cloudbaristaorg/cb-dragonfly:0.7.0
 
 # MCIS Collector Image
 sudo docker build -t cloudbaristaorg/cb-dragonfly:0.7.0-mcis-collector -f Collector.MCIS.Dockerfile . --no-cache
 sudo docker push cloudbaristaorg/cb-dragonfly:0.7.0-mcis-collector
 
 # MCKS Collector Image
-sudo docker build -t cloudbaristaorg/cb-dragonfly:0.7.0-mcks-collector -f Collector.MCKS.Dockerfile . --no-cache
-sudo docker push cloudbaristaorg/cb-dragonfly:0.7.0-mcks-collector
+sudo docker build -t cloudbaristaorg/cb-dragonfly:0.7.0-mck8s-collector -f Collector.MCKS.Dockerfile . --no-cache
+sudo docker push cloudbaristaorg/cb-dragonfly:0.7.0-mck8s-collector
