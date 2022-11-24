1726a65bfcfee38be9844289541f5a3d2478bac3
snchoi <sn0716@naver.com>
2022-11-24 09:11:16 +0900

    add service type config
    * add service type config
    * healthcheck logic change in aggregate
    * telegraf file change

--- pkg/api/grpc/server/monitoring.go
+++ pkg/api/grpc/server/monitoring.go
@@ -181,7 +181,7 @@ func (c MonitoringService) GetVMOnDemandMonNetworkInfo(ctx context.Context, requ
 func (c MonitoringService) GetVMMonCpuInfo(ctx context.Context, request *pb.VMMonQryRequest) (*pb.CpuInfoResponse, error) {
 	monReqInfo := types.DBMetricRequestInfo{
 		NsID:                request.NsId,
-		ServiceType:         types.MCIS,
+		ServiceType:         request.ServiceType,
 		ServiceID:           request.McisId,
 		VMID:                request.VmId,
 		MetricName:          types.Cpu.ToString(),
@@ -220,7 +220,7 @@ func (c MonitoringService) GetVMMonCpuInfo(ctx context.Context, request *pb.VMMo
 func (c MonitoringService) GetVMMonCpuFreqInfo(ctx context.Context, request *pb.VMMonQryRequest) (*pb.CpuFreqInfoResponse, error) {
 	monReqInfo := types.DBMetricRequestInfo{
 		NsID:                request.NsId,
-		ServiceType:         types.MCIS,
+		ServiceType:         request.ServiceType,
 		ServiceID:           request.McisId,
 		VMID:                request.VmId,
 		MetricName:          types.CpuFrequency.ToString(),
@@ -258,7 +258,7 @@ func (c MonitoringService) GetVMMonCpuFreqInfo(ctx context.Context, request *pb.
 func (c MonitoringService) GetVMMonMemoryInfo(ctx context.Context, request *pb.VMMonQryRequest) (*pb.MemoryInfoResponse, error) {
 	monReqInfo := types.DBMetricRequestInfo{
 		NsID:                request.NsId,
-		ServiceType:         types.MCIS,
+		ServiceType:         request.ServiceType,
 		ServiceID:           request.McisId,
 		VMID:                request.VmId,
 		MetricName:          types.Memory.ToString(),
@@ -296,7 +296,7 @@ func (c MonitoringService) GetVMMonMemoryInfo(ctx context.Context, request *pb.V
 func (c MonitoringService) GetVMMonDiskInfo(ctx context.Context, request *pb.VMMonQryRequest) (*pb.DiskInfoResponse, error) {
 	monReqInfo := types.DBMetricRequestInfo{
 		NsID:                request.NsId,
-		ServiceType:         types.MCIS,
+		ServiceType:         request.ServiceType,
 		ServiceID:           request.McisId,
 		VMID:                request.VmId,
 		MetricName:          types.Disk.ToString(),
@@ -334,7 +334,7 @@ func (c MonitoringService) GetVMMonDiskInfo(ctx context.Context, request *pb.VMM
 func (c MonitoringService) GetVMMonNetworkInfo(ctx context.Context, request *pb.VMMonQryRequest) (*pb.NetworkInfoResponse, error) {
 	monReqInfo := types.DBMetricRequestInfo{
 		NsID:                request.NsId,
-		ServiceType:         types.MCIS,
+		ServiceType:         request.ServiceType,
 		ServiceID:           request.McisId,
 		VMID:                request.VmId,
 		MetricName:          types.Network.ToString(),
